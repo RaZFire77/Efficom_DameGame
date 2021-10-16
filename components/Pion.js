ce332a37ae99d7e77526e3d575f0420ee998cadf
Anthony <mranthony.ra@gmail.com>
2021-10-16 11:15:14 +0200

    fix display

--- components/Pion.js
+++ components/Pion.js
@@ -1,10 +1,15 @@
 import React from "react"
+import classNames from "classnames"
 
 function Pion({ color, position, askPos }) {
   return (
     <button
       onClick={() => askPos(color, position)}
-      className={`w-6 h-6 rounded-full bg-${color}`}
+      className={classNames(
+        "w-6 h-6 rounded-full bg",
+        { "bg-white": color === "white" },
+        { "bg-black": color === "black" }
+      )}
     />
   )
 }
